2800a1d9a39a107b11e798e49068df2d5d3d351e
Liesy <preposterous@163.com>
2021-11-12 14:36:36 +0800

    chore: 11.12 exp8 need to improve.

--- MatchingAlgorithm.cpp
+++ MatchingAlgorithm.cpp
@@ -12,18 +12,33 @@ Mat src_img_original, src_gray_original;
 Mat src_img_scale, src_gray_scale;
 Mat src_img_local, src_gray_local;
 
-vector<KeyPoint> KP_original, KP_scale, KP_local;
-Mat desc_original, desc_scale, desc_local;
-
-double t, t1, t2;
-
 void showImage(Mat &image, const char *windowName, int location_x, int location_y) {
     namedWindow(windowName, WINDOW_AUTOSIZE);
     moveWindow(windowName, location_x, location_y);
     imshow(windowName, image);
 }
 
-void matching(const char *algorithmName) {
+void siftMatching() {
+    double start, end;
+
+    //SIFT特征点提取
+    Ptr<FeatureDetector> siftDetector = SIFT::create(400);
+    vector<KeyPoint> KP_original, KP_scale, KP_local;
+    start = (double) getTickCount();
+    siftDetector->detect(src_gray_original, KP_original);
+    siftDetector->detect(src_gray_scale, KP_scale);
+    siftDetector->detect(src_gray_local, KP_local);
+    end = (double) getTickCount();
+    cout << "SIFT finished in " << ((end - start) * 1000.0) / getTickFrequency() << "ms" << '\n';
+
+    //特征点描述
+    Ptr<DescriptorExtractor> siftDescriptor = SIFT::create(2000);
+    Mat desc_original, desc_scale, desc_local;
+    siftDescriptor->compute(src_gray_original, KP_original, desc_original);
+    siftDescriptor->compute(src_gray_scale, KP_scale, desc_scale);
+    siftDescriptor->compute(src_gray_local, KP_local, desc_local);
+
+    //获得匹配特征点
     FlannBasedMatcher scaleMatcher, localMatcher;
     vector<DMatch> scaleMatchPts, localMatchPts;
     scaleMatcher.match(desc_original, desc_scale, scaleMatchPts);
@@ -31,36 +46,44 @@ void matching(const char *algorithmName) {
 
     Mat img_scaleMatch, img_localMatch;
     drawMatches(src_img_original, KP_original, src_img_scale, KP_scale, scaleMatchPts, img_scaleMatch);
-    drawMatches(src_img_original, KP_original, src_img_local, KP_scale, localMatchPts, img_localMatch);
+    drawMatches(src_img_original, KP_original, src_img_local, KP_local, localMatchPts, img_localMatch);
 
-//    showImage(img_scaleMatch, algorithmName, 200, 200);
-//    showImage(img_localMatch, algorithmName, 400, 200);
+    showImage(img_scaleMatch, "sift scale matching", 200, 200);
+    showImage(img_localMatch, "sift local matching", 800, 200);
 }
 
-void featureProcess() {
-    Ptr<SIFT> sift = SIFT::create(2000);
-    Ptr<ORB> orb = ORB::create(2000);
-    t = (double) getTickCount();
+void orbMatching() {
+    double start, end;
+
     //SIFT特征点提取
-    sift->detect(src_gray_original, KP_original);
-    sift->detect(src_gray_scale, KP_scale);
-    sift->detect(src_gray_local, KP_local);
+    Ptr<FeatureDetector> orbDetector = ORB::create(400);
+    vector<KeyPoint> KP_original, KP_scale, KP_local;
+    start = (double) getTickCount();
+    orbDetector->detect(src_gray_original, KP_original);
+    orbDetector->detect(src_gray_scale, KP_scale);
+    orbDetector->detect(src_gray_local, KP_local);
+    end = (double) getTickCount();
+    cout << "orb finished in " << ((end - start) * 1000.0) / getTickFrequency() << "ms" << '\n';
+
     //特征点描述
-    sift->compute(src_gray_original, KP_original, desc_original);
-    sift->compute(src_gray_scale, KP_scale, desc_scale);
-    sift->compute(src_gray_local, KP_local, desc_local);
-    matching("sift");
-    t1 = (double) getTickCount();
-    //ORB特征点提取
-    orb->detect(src_gray_original, KP_original);
-    orb->detect(src_gray_scale, KP_scale);
-    orb->detect(src_gray_local, KP_local);
-    //特征点描述
-    orb->compute(src_gray_original, KP_original, desc_original);
-    orb->compute(src_gray_scale, KP_scale, desc_scale);
-    orb->compute(src_gray_local, KP_local, desc_local);
-    matching("orb");
-    t2 = (double) getTickCount();
+    Ptr<DescriptorExtractor> orbDescriptor = ORB::create(2000);
+    Mat desc_original, desc_scale, desc_local;
+    orbDescriptor->compute(src_gray_original, KP_original, desc_original);
+    orbDescriptor->compute(src_gray_scale, KP_scale, desc_scale);
+    orbDescriptor->compute(src_gray_local, KP_local, desc_local);
+
+    //获得匹配特征点
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
+    vector<DMatch> scaleMatchPts, localMatchPts;
+    matcher->match(desc_original, desc_scale, scaleMatchPts);
+    matcher->match(desc_original, desc_local, localMatchPts);
+
+    Mat img_scaleMatch, img_localMatch;
+    drawMatches(src_img_original, KP_original, src_img_scale, KP_scale, scaleMatchPts, img_scaleMatch);
+    drawMatches(src_img_original, KP_original, src_img_local, KP_local, localMatchPts, img_localMatch);
+
+    showImage(img_scaleMatch, "orb scale matching", 200, 200);
+    showImage(img_localMatch, "orb local matching", 800, 200);
 }
 
 int main() {
@@ -73,9 +96,8 @@ int main() {
     cvtColor(src_img_scale, src_gray_scale, COLOR_BGR2GRAY);
     cvtColor(src_img_local, src_gray_local, COLOR_BGR2GRAY);
 
-    featureProcess();
-    cout << "SIFT finished in " << ((t1 - t) * 1000.0) / getTickFrequency() << "ms" << '\n';
-    cout << "ORB finished in " << ((t2 - t1) * 1000.0) / getTickFrequency() << "ms" << '\n';
+    siftMatching();
+    orbMatching();
 
     waitKey(0);
     destroyAllWindows();
